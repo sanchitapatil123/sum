061b6b38db94ef2d1a1f6db5b53a2151a081b886
sanchitapatil123 <patilsanchita025@gmail.com>
2025-04-08 12:03:01 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 5; // First number
+        int b = 10; // Second number
+        int sum = a + b; // Adding the numbers
+
+        System.out.println("The sum of two numbers is " + sum); // Printing the output
+    }
+}
